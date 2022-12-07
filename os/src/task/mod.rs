c0aac26d882b7f82a38a798e28f7e0c7021e59d3
NagleZhang <1247555725@qq.com>
2022-12-07 16:00:11 +0800

    add task manager code.

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -42,13 +42,66 @@ lazy_static! {
 impl TaskManager {
     fn mark_current_suspended(&self) {
         let mut inner = self.inner.borrow_mut();
+        let current = inner.current_task;
+        inner.tasks[current].task_status = TaskStatus::Ready;
+    }
+
+    fn mark_current_exited(&self) {
+        let mut inner = self.inner.borrow_mut();
+        let current = inner.current_task;
+        inner.tasks[current].task_status = TaskStatus::Exited;
+    }
+
+    // put latest task
+    fn run_next_task(&self) {
+        // find next runnable application
+        if let Some(next) = find_next_task() {
+            let mut inner = self.inner.exclusive_access();
+            let current =  inner.current_task;
+
+            inner.tasks[next].task_status = TaskStatus::Running;
+            inner.current_task = next;
+
+            let current_task_cx_ptr = &mut inner.tasks[current].task_cx as *mut TaskContext;
+            let next_task_cx_ptr = &inner.tasks[next].taxk_cx as *const TaskContext;
+
+            drop(inner);
+
+            unsafe{
+                __switch(
+                    current_task_cx_ptr,
+                    next_task_cx_ptr,
+                );
+            }
+        } else {
+            panic!("All application completed!");
+        }
+    }
+
+    fn find_next_task(&self) -> Option<usize> {
+        let inner = self.inner.exclusive_access();
+        let current = inner.current_task;
+
+        (current+1 .. current+self.app_count+1)
+            .map(|id| id % self.app_count)
+            .find(|id| {
+                inner.tasks[*id].tasks_status == TaskStatus::Ready
+            })
+
+        //for i in 0..self.app_count {
+        //    if inner.tasks[i].tasks_status == TaskStatus::Ready {
+        //        i
+        //    }
+        //}
     }
 }
 
 pub fn mark_current_suspended() {
-    TASK_MANAGER.
+    TASK_MANAGER.mark_current_suspended();
 }
-pub fn mark_current_exited() {}
+pub fn mark_current_exited() {
+    TASK_MANAGER.mark_current_exited();
+}
 
 pub fn suspend_current_and_run_next() {
     mark_current_suspend();
@@ -60,4 +113,6 @@ pub fn exit_current_and_run_next() {
     run_next_task();
 }
 
-
+fn run_next_task() {
+    TASK_MANAGER.run_next_task();
+}
