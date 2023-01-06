e8e7b7f794fef89be7c35301510ff09385030cd0
NagleZhang <1247555725@qq.com>
2023-01-06 23:30:33 +0800

    delete unneed trap code.

--- os/src/trap/mod.rs
+++ os/src/trap/mod.rs
@@ -10,89 +10,7 @@ use riscv::register::{
     stval, stvec, sie,
 };
 
-/*
-use riscv::register::{
-    mtvec::TrapMode,
-    scause::{self, Exception, Interrupt, Trap},
-    sie, stval, stvec,
-};
-*/
-
 core::arch::global_asm!(include_str!("trap.S"));
-
-/*
-pub fn init() {
-    extern "C"  { fn __alltraps(); }
-    unsafe {
-        // stvec register point to address which handler trap
-        stvec::write(__alltraps as usize, TrapMode::Direct);
-    }
-}
-
-*/
-/*
-what should trap handler do?
-remember this function is called by __alltraps, so register are saved.
-and currently we have traped into supervisor mode.
-what we need to do is dispatch syscalls
-#[no_mangle]
-pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
-    let scause = scause::read();
-    let stval = stval::read();
-
-    match scause.cause() {
-        Trap::Exception(Exception::UserEnvCall) => {
-            cx.sepc += 4;
-            cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
-        }
-        Trap::Exception(Exception::StoreFault) |
-        Trap::Exception(Exception::StorePageFault) => {
-            println!("[kernel] PageFault in application, core dumped.");
-            run_next_app();
-        }
-        Trap::Exception(Exception::IllegalInstruction) => {
-            println!("[kernel] IllegalInstruction in application, core dumped.");
-            run_next_app();
-        }
-        _ => {
-            panic!("Unsupported trap {:?}, sval = {:#x}!", scause.cause(), stval);
-        }
-    }
-    cx
-}
- */
-/*
-#[no_mangle]
-pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
-    let scause = scause::read();
-    let stval = stval::read();
-    match scause.cause() {
-        Trap::Exception(Exception::UserEnvCall) => {
-            cx.sepc += 4;
-            cx.x[10] = syscall(cx.x[17], [cx.x[10], cx.x[11], cx.x[12]]) as usize;
-        }
-        Trap::Exception(Exception::StoreFault) | Trap::Exception(Exception::StorePageFault) => {
-            error!("[kernel] PageFault in application, core dumped.");
-            run_next_app();
-        }
-        Trap::Exception(Exception::IllegalInstruction) => {
-            error!("[kernel] IllegalInstruction in application, core dumped.");
-            run_next_app();
-        }
-        _ => {
-            panic!();
-            //    "Unsupported trap {:?}, stval = {:#x}!",
-            //    scause.cause(),
-            //    stval
-            //);
-        }
-    }
-    cx
-}
-
-
-pub use context::TrapContext;
-*/
 
 pub fn init() {
     extern "C" {
@@ -121,13 +39,11 @@ pub fn trap_handler(cx: &mut TrapContext) -> &mut TrapContext {
         Trap::Exception(Exception::StoreFault) | Trap::Exception(Exception::StorePageFault) => {
             error!("[kernel] PageFault in application, core dumped.");
             exit_current_and_run_next();
-            panic!();
             //run_next_app();
         }
         Trap::Exception(Exception::IllegalInstruction) => {
             error!("[kernel] IllegalInstruction in application, core dumped.");
             exit_current_and_run_next();
-            panic!();
             //run_next_app();
         }
         Trap::Interrupt(Interrupt::SupervisorTimer) => {
